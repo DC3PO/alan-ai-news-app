24819bea44105e4b4d03d57744c8cfdd79528273
dc3po0 <dc3po0@gmail.com>
2020-08-25 11:16:46 -0600

    adding styling to the home page and news cards

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles.js';
 
 const alanKey = 'de0405998380d8f370edccd1e8ee77242e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([]);
+    const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
@@ -21,7 +23,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>Alan AI News Application</h1>
+            <div className={classes.logoContainer}>
+                <img src="http://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo" />
+            </div>
             <NewsCards articles={newsArticles} />
         </div>
     );
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -7,19 +7,19 @@ const NewsCard = ({ article: { description, publishedAt, source, title, url, url
     const classes = useStyles();
     
     return (
-        <Card>
-            <CardActionArea>
+        <Card className={classes.details}>
+            <CardActionArea href={url} target="_blank">
                 <CardMedia className={classes.media} image={urlToImage || 'https://cdn.pixabay.com/photo/2015/02/15/09/33/news-636978_1280.jpg'} />
-                <div>
-                <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
-                <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+                <div className={classes.card}>
+                    <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
                 </div>
-                <Typography gutterBottom variant="h5">{title}</Typography>
+                <Typography className={classes.title} gutterBottom variant="h5">{title}</Typography>
                 <CardContent>
                     <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
                 </CardContent>
             </CardActionArea>
-            <CardActions>
+            <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary">Learn More</Button>
                 <Typography variant="h5" color="textSecondary">{i + 1}</Typography>  
             </CardActions>
